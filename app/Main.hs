97490ba684dfd4ea8bbad725e98aed6467492453
Octavio <ruivaldelvalle@gmail.com>
2025-04-28 11:46:34 -0300

    Agrego poder lluvia de tuercas

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,20 @@
 module Main () where
 
-    type personaje=(String, String, String, Bool, Int)
+    type Personaje=(String, String, String, Bool, Int)
 
-bolaEspinosa :: personaje -> personaje
+
+
+bolaEspinosa :: Personaje -> Personaje
  bolaEspinosa (_ , _ , _ , _ , vida)
     |    vida >1000 = vida -1000
     |  otherwise = vida - vida
 
+esAliado :: Personaje -> Bool
+esAliado unPersonaje = True
+
+lluviaDeTuercas :: personaje -> personaje
+lluviaDeTuercas (_ , _ , _ , _ , vida)
+  | esAliado unPersonaje = vida + 800
+  | not.esAliado unPersonaje = div vida 2
+  | otherwise unPersonaje = unPersonaje
 
