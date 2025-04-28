81d5f7a89593fb2829327db53e1681327e11283d
Octavio <ruivaldelvalle@gmail.com>
2025-04-28 11:28:31 -0300

    Agrego primer poder bola espinosa

--- app/Main.hs
+++ app/Main.hs
@@ -1,1 +1,10 @@
 module Main () where
+
+    type personaje=(String, String, String, Bool, Int)
+
+bolaEspinosa :: personaje -> personaje
+ bolaEspinosa (_ , _ , _ , _ , vida)
+    |    vida >1000 = vida -1000
+    |  otherwise = vida - vida
+
+
