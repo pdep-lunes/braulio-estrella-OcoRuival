8789902937b8cd9756248c76d9a4354f04801263
Octa <ruivaldelvalle@gmail.com>
2025-05-04 23:02:05 -0300

    Subo los poderes especiales personajes y reportes

--- app/Main.hs
+++ app/Main.hs
@@ -3,25 +3,51 @@ module Main () where
     data Personaje= unPersonaje{
         nombre :: String
         poderBasico :: String
+        superPoder :: String
         superPoderActivo :: Bool
         cantidadDeVida :: Int
+        radioDeAtaque :: Int
         }
 
-esPoder :: 
+data Equipo = unEquipo{
+  primerCompanero :: Personaje
+  segundoCompanero :: Personaje
+  numeroDeEquipo :: Int
+}
+  
+estaEnElMismoEquipo :: Equipo -> Personaje -> Bool
+estaEnElMismoEquipo equipo enemigo = (personaje1 equipo == enemigo) || (personaje2 equipo == enemigo)
 
 bolaEspinosa :: Personaje -> Personaje
-bolaEspinosa (_ , _ , _ , _ , vida)
-    |    vida >1000 = vida -1000
-    |  otherwise = vida - vida
+bolaEspinosa unPersonaje
+    |    cantidadDeVida unPersonaje >1000 = unPersonaje{ cantidadDeVida = cantidadDeVida unPersonaje - 1000}
+    |  otherwise =  unPersonaje{ cantidadDeVida = cantidadDeVida unPersonaje - cantidadDeVida unPersonaje}
 
 
-lluviaDeTuercas :: Personaje -> Personaje
-lluviaDeTuercas (_ , _ , _ , _ , vida)
-  | unPersonaje = vida + 800
-  | unPersonaje = div vida 2
-  | otherwise unPersonaje = unPersonaje
+
+lluviaDeTuercas :: Equipo -> Personaje -> Personaje
+lluviaDeTuercas equipo enemigo
+  |  estaEnElMismoEquipo enemigo = cantidadDeVida enemigo + 800
+  | otherwise = div cantidadDeVida enemigo 2
+  
  
 
-granadaDeEspinas :: Personaje -> Int -> Personaje
-granadaDeEspinas (nombre , _ , _ , superActiva , vida) radioDeExplosion 
-  | radioDeExplosion > 3 = (nombre )
+granadaDeEspinas :: Personaje -> Personaje -> Int -> Personaje
+granadaDeEspinas unPersonaje enemigo radioDeExplosionAtacante
+  | radioDeExplosionAtacante > 3  = unPersonaje {nombre = nombre unPersonaje ++ "Espina estuvo aqui"}
+  | cantidadDeVida enemigo < 800 = enemigo { cantidadDeVida = 0, superPoderActivo = False}
+  | otherwise = bolaEspinosa enemigo
+
+torretaCurativa :: Personaje -> Personaje -> Personaje
+torretaCurativa unPersonaje otroPersonaje = otroPersonaje {superPoderActivo = True, cantidadDeVida = cantidadDeVida*2}
+
+atacarConElPoderEspecial :: Personaje -> Personaje -> Personaje
+atacarConElPoderEspecial unPersonaje contrincante
+  | unPersonaje {superPoderActivo == True} = superPoder 
+
+estaEnLasUltimas :: Personaje -> Bool
+estaEnLasUltimas unPersonaje = vida unPersonaje < 800
+
+espina = Personaje "Espina" "Bola de Espinas" "Granada de Espinas" True 4800 5
+pamela = Personaje "Pamela" "Lluvia de Tuercas" "Torreta curativa" False 9600 0
+  
